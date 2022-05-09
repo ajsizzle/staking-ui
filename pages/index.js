b4f36dffec3aedf21f783bcd1909991c15a0c209
Alex Johnson <ajohnson2389@gmail.com>
2022-05-09 15:02:57 -0400

    added staking form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import styles from "../styles/Home.module.css"
 import Header from "../components/Header"
 import StakeDetails from "../components/StakeDetails"
+import StakeForm from "../components/StakedForm"
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Header />
             <StakeDetails />
+            <StakeForm />
         </div>
     )
 }
